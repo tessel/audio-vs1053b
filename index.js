9524b456e75f4eff780217eecbe0f494aeb2fc7c
Tim Cameron Ryan <id@timryan.org>
2014-01-12 13:13:59 -0500

    Preliminary audio code and sample

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-var tm = process.binding('tm');
+var tessel = require('tessel');
+var fs = require('fs');
 
-var MP3_XCS = tm.PIN_A_G1 //Control Chip Select Pin (for accessing SPI Control/Status registers)
-var MP3_DCS = tm.PIN_A_G3 //Data Chip Select / BSYNC Pin
-var MP3_DREQ = tm.PIN_A_G2 //Data Request Pin: Player asks for more data
 
 //VS10xx SCI Registers
 var SCI_MODE = 0x00
@@ -22,25 +20,41 @@ var SCI_AICTRL1 = 0x0D
 var SCI_AICTRL2 = 0x0E
 var SCI_AICTRL3 = 0x0F
 
+
+var hair = fs.readFileSync('/app/twinfalls.mp3');
+
+var port = tessel.port('a');
+
+
+var MP3_XCS = port.gpio(1).input() //Control Chip Select Pin (for accessing SPI Control/Status registers)
+var MP3_DCS = port.gpio(2).output().high() //Data Chip Select / BSYNC Pin
+var MP3_DREQ = port.gpio(3).output().high() //Data Request Pin: Player asks for more data
+
+var spi = new port.SPI({
+  clockSpeed: 1000000,
+  dataMode: tessel.SPIDataMode.Mode0
+})
+
+
 function SPItransfer (byte) {
-  return tm.spi_transfer(tm.SPI_0, [byte])[0];
+  return spi.transferSync([byte])[0];
 }
 
 //Read the 16-bit value of a VS10xx register
 function Mp3ReadRegister (addressbyte){
-  while(!tm.pin_read_digital(MP3_DREQ)) ; //Wait for DREQ to go high indicating IC is available
-  tm.pin_write_digital(MP3_XCS, tm.PIN_LOW); //Select control
+  while (!MP3_DREQ.read()) ; //Wait for DREQ to go high indicating IC is available
+  MP3_XCS.low(); //Select control
 
   //SCI consists of instruction byte, address byte, and 16-bit data word.
   SPItransfer(0x03); //Read instruction
   SPItransfer(addressbyte);
 
   var response1 = SPItransfer(0xFF);
-  while(!tm.pin_read_digital(MP3_DREQ)) ; //Wait for DREQ to go high indicating command is complete
+  while (!MP3_DREQ.read()) ; //Wait for DREQ to go high indicating command is complete
   var response2 = SPItransfer(0xFF); //Read the second byte
-  while(!tm.pin_read_digital(MP3_DREQ)) ; //Wait for DREQ to go high indicating command is complete
+  while (!MP3_DREQ.read()) ; //Wait for DREQ to go high indicating command is complete
 
-  tm.pin_write_digital(MP3_XCS, tm.PIN_HIGH); //Deselect Control
+  MP3_XCS.high(); //Deselect Control
 
   var resultvalue = response1 << 8;
   resultvalue = resultvalue + response2;
@@ -48,40 +62,22 @@ function Mp3ReadRegister (addressbyte){
 }
 
 function Mp3WriteRegister(addressbyte, highbyte, lowbyte){
-  while(!tm.pin_read_digital(MP3_DREQ)) ; //Wait for DREQ to go high indicating IC is available
-  tm.pin_write_digital(MP3_XCS, tm.PIN_LOW); //Select control
+  while(!MP3_DREQ.read()) ; //Wait for DREQ to go high indicating IC is available
+  MP3_XCS.low(); //Select control
 
   //SCI consists of instruction byte, address byte, and 16-bit data word.
   SPItransfer(0x02); //Write instruction
   SPItransfer(addressbyte);
   SPItransfer(highbyte);
   SPItransfer(lowbyte);
-  while(!tm.pin_read_digital(MP3_DREQ)) ; //Wait for DREQ to go high indicating command is complete
-  tm.pin_write_digital(MP3_XCS, tm.PIN_HIGH); //Deselect Control
+  while(!MP3_DREQ.read()) ; //Wait for DREQ to go high indicating command is complete
+  MP3_XCS.high(); //Deselect Control
 }
 
 //Set VS10xx Volume Register
 function Mp3SetVolume (leftchannel, rightchannel){
   Mp3WriteRegister(SCI_VOL, leftchannel, rightchannel);
 }
-
-tm.pin_mode(MP3_DREQ, tm.PIN_INPUT);
-tm.pin_mode(MP3_XCS, tm.PIN_OUTPUT);
-tm.pin_mode(MP3_DCS, tm.PIN_OUTPUT);
-
-//Setup SPI for VS1053
-//  pinMode(10, OUTPUT); //Pin 10 must be set as an output for the SPI communication to work
-tm.spi_initialize(tm.SPI_0);
-// tm_spi_bitorder_set(TM_SPI_0, MSBFIRST);
-tm.spi_datamode_set(tm.SPI_0, tm.SPI_MODE_0);
-
-//From page 12 of datasheet, max SCI reads are CLKI/7. Input clock is 12.288MHz.
-//Internal clock multiplier is 1.0x after power up.
-//Therefore, max SPI speed is 1.75MHz. We will use 1MHz to be safe.
-tm.spi_clockspeed_set(tm.SPI_0, 1000000); //Set SPI bus speed to 1MHz (16MHz / 16 = 1MHz)
-
-tm.pin_write_digital(MP3_XCS, tm.PIN_HIGH); //Deselect Control
-tm.pin_write_digital(MP3_DCS, tm.PIN_HIGH); //Deselect Control
 
 console.log("MP3 Shield Example");
 SPItransfer(0xFF); //Throw a dummy byte at the bus
@@ -100,43 +96,30 @@ console.log("VS Version (VS1053 is 4) = ", vsVersion);
 
 
 Mp3WriteRegister(SCI_CLOCKF, 0x60, 0x00); //Set multiplier to 3.0x
-tm.spi_clockspeed_set(tm.SPI_0, 4000000); //Set SPI bus speed to 4MHz (16MHz / 4 = 4MHz)
+spi.setClockSpeed(4000000); //Set SPI bus speed to 4MHz (16MHz / 4 = 4MHz)
 
 var MP3Clock = Mp3ReadRegister(SCI_CLOCKF);
 console.log("SCI_ClockF = 0x", MP3Clock.toString(16));
 
-var len = tm.wethair_mp3_len;
-tm.pin_write_digital(MP3_DCS, tm.PIN_LOW);
-for (var p = 0; p < len; p = p + 32) {
-  // while (!tm.pin_read_digital(MP3_DREQ)) { continue; }
+console.log('chunking.');
+
+var len = hair.length;
+var chunks = [], clen = 2048;
+for (var p = 0; p < len; p = p + clen) {
+  chunks.push(hair.substr(p, clen));
+  // console.log(p);
   // tm.pin_write_digital(MP3_DCS, tm.PIN_LOW);
-  tm.spi_send(tm.SPI_0, tm.wethair_mp3.substr(p, 32));
+  // spi.transferSync(hair.substr(p, 32));
   // tm.pin_write_digital(MP3_DCS, tm.PIN_HIGH);
 }
-console.log('done');
 
-/*
-while(p <= &HelloMP3[sizeof(HelloMP3) - 1]) {
-  while(!digitalRead(MP3_DREQ)) {
-    //DREQ is low while the receive buffer is full
-    //You can do something else here, the bus is free...
-    //Maybe set the volume or whatever...
-  }
+console.log('done chunking.');
 
-  //Once DREQ is released (high) we can now send 32 bytes of data
-  digitalWrite(MP3_DCS, LOW); //Select Data
-  while(!digitalRead(MP3_DREQ)); //If we ever see DREQ low, then we wait here
-  tm_spi_send(TM_SPI_0, p, 32);
-  p += 32;
-  digitalWrite(MP3_DCS, HIGH); //Deselect Data
+MP3_DCS.low();
+var i = 0, len = chunks.length;
+while (i < len) {
+  // while (!MP3_DREQ.read()) { }
+  spi.send(chunks[i]);
+  i = i + 1;
 }
-
-//End of file - send 2048 zeros before next file
-digitalWrite(MP3_DCS, LOW); //Select Data
-for (int i = 0 ; i < 2048 ; i++) {
-  while(!digitalRead(MP3_DREQ)); //If we ever see DREQ low, then we wait here
-  SPItransfer(0);
-}
-while(!digitalRead(MP3_DREQ)) ; //Wait for DREQ to go high indicating transfer is complete
-digitalWrite(MP3_DCS, HIGH); //Deselect Data
-*/
+console.log('done playing.');
