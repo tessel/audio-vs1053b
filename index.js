a7896c8e700d375be30391866666d02088b56496
johnnyman727 <jonathan.mckay@students.olin.edu>
2014-04-03 18:04:31 -0400

    Shifted code over to async methods

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // VS1053b module
 
 var fs = require('fs');
+var events = require('events');
+var util = require('util');
 
 // VS10xx SCI Registers
-
 var SCI_MODE = 0x00
   , SCI_STATUS = 0x01
   , SCI_BASS = 0x02
@@ -19,264 +20,309 @@ var SCI_MODE = 0x00
   , SCI_AICTRL0 = 0x0C
   , SCI_AICTRL1 = 0x0D
   , SCI_AICTRL2 = 0x0E
-  , SCI_AICTRL3 = 0x0F
+  , SCI_AICTRL3 = 0x0F;
 
-function connect (port)
-{
-  var MP3_XCS = port.gpio(1).output().high() //Control Chip Select Pin (for accessing SPI Control/Status registers)
-  var MP3_DCS = port.gpio(2).output().high() //Data Chip Select / BSYNC Pin
-  var MP3_DREQ = port.gpio(3).input() //Data Request Pin: Player asks for more data
 
-  var spi = new port.SPI({
+var GPIO_DIR_ADDR = 0xC017
+  , GPIO_READ_ADDR = 0xC018
+  , GPIO_SET_ADDR = 0xC019;
+
+function use (hardware, next) {
+  return new Audio(hardware, next);
+}
+
+
+function Audio(hardware, callback) {
+  // Set the spi port
+  this.spi = new hardware.SPI({
     clockSpeed: 1000000,
     dataMode: 0
-  })
+  });
 
+  // Set our register select pins
+  this.MP3_XCS = hardware.gpio(1).output().high() //Control Chip Select Pin (for accessing SPI Control/Status registers)
+  this.MP3_DCS = hardware.gpio(2).output().high() //Data Chip Select / BSYNC Pin
+  this.MP3_DREQ = hardware.gpio(3).input() //Data Request Pin: Player asks for more data
 
-  function SPItransfer (byte)
-  {
-    return spi.transferSync(new Buffer([byte]))[0];
-  }
+  this.input = "";
+  this.output = "";
+  this.initialize(callback);
+}
 
-  //Read the 16-bit value of a VS10xx register
-  function readSciRegister16 (addressbyte)
-  {
-    while (!MP3_DREQ.read()) ; //Wait for DREQ to go high indicating IC is available
-    MP3_XCS.low(); //Select control
+util.inherits(Audio, events.EventEmitter);
 
-    //SCI consists of instruction byte, address byte, and 16-bit data word.
-    SPItransfer(0x03); //Read instruction
-    SPItransfer(addressbyte);
+Audio.prototype.initialize = function(callback) {
+  var self = this;
 
-    var response1 = SPItransfer(0xFF);
-    while (!MP3_DREQ.read()) ; //Wait for DREQ to go high indicating command is complete
-    var response2 = SPItransfer(0xFF); //Read the second byte
-    while (!MP3_DREQ.read()) ; //Wait for DREQ to go high indicating command is complete
-
-    MP3_XCS.high(); //Deselect Control
-
-    var resultvalue = response1 << 8;
-    resultvalue = resultvalue + response2;
-
-    return resultvalue;
-  }
-
-  function writeSciRegister (addressbyte, highbyte, lowbyte)
-  {
-    while(!MP3_DREQ.read()) ; //Wait for DREQ to go high indicating IC is available
-    MP3_XCS.low(); //Select control
-
-    //SCI consists of instruction byte, address byte, and 16-bit data word.
-    SPItransfer(0x02); //Write instruction
-    SPItransfer(addressbyte);
-    SPItransfer(highbyte);
-    SPItransfer(lowbyte);
-    while(!MP3_DREQ.read()) ; //Wait for DREQ to go high indicating command is complete
-    MP3_XCS.high(); //Deselect Control
-  }
-
-  function writeSciRegister16 (addressbyte, word)
-  {
-    writeSciRegister (addressbyte, (word >> 8) & 0xFF, word & 0xFF);
-  }
-
-  function setVolume (leftchannel, rightchannel)
-  {
-    // Set VS10xx Volume Register
-    writeSciRegister(SCI_VOL, leftchannel, rightchannel);
-  }
-
-
-  // VS1053b generic chip gpio functions
-
-  function setChipGpioDir (arg) {
-    writeSciRegister(SCI_WRAMADDR, 0xC0, 0x17);
-    writeSciRegister(SCI_WRAM, 0x00, arg);
-  }
-
-  function setChipGpio (arg) {
-    writeSciRegister(SCI_WRAMADDR, 0xC0, 0x19);
-    writeSciRegister(SCI_WRAM, 0x00, arg)
-  }
-
-
-  // Tessel-specific headphones/mic options
-
-  var useLineOut = 0;
-  var useLineIn = 0;
-
-  function _setChipGpioDir () {
-    setChipGpioDir((1 << 7) + (1 << 5)) // enable GPIO5 (line in) and GPIO7 (headphones/ext amp)
-  }
-
-  function _setChipGpio () {
-    setChipGpio((useLineOut << 7) + (useLineIn << 5));
-  }
-
-  function enableHeadphones () {
-    useLineOut = 0;
-    _setChipGpio();
-  }
-
-  function enableLineOut (on) {
-    useLineOut = 1;
-    _setChipGpio();
-  }
-
-  function enableMicrophone (on) {
-    useLineIn = 0;
-    _setChipGpio()
-  }
-
-  function enableLineIn (on) {
-    useLineIn = 1;
-    _setChipGpio();
-  }
-
-
-  function initialize ()
-  {
-    console.log("Tessel Audio Module");
-    SPItransfer(0xFF); //Throw a dummy byte at the bus
-
-    // Soft reset
-    console.log('Soft reset');
-    writeSciRegister16(SCI_MODE, 0x4800 | 2)
-
-    setVolume(40, 40); //Set initial volume (20 = -10dB)
-    writeSciRegister(SCI_MODE, 0x48, 0x00);
-
-    //Let's check the status of the VS1053
-    var MP3Mode = readSciRegister16(SCI_MODE);
-    console.log("SCI_Mode (0x4800) = 0x", MP3Mode.toString(16));
-
-    var MP3Status = readSciRegister16(SCI_STATUS);
-    var vsVersion = (MP3Status >> 4) & 0x000F; //Mask out only the four version bits
-    console.log("VS Version (VS1053 is 4) = ", vsVersion);
-    //The 1053B should respond with 4. VS1001 = 0, VS1011 = 1, VS1002 = 2, VS1003 = 3
-
-    // Toggle GPIOs
-    _setChipGpioDir();
-    enableHeadphones();
-    enableMicrophone();
-    writeSciRegister(SCI_WRAMADDR, 0xC0, 0x19);
-    console.log('GPIOS = (0)', readSciRegister16(SCI_WRAM));
-
-    // Set multiplier etc
-
-    writeSciRegister(SCI_CLOCKF, 0x60, 0x00); //Set multiplier to 3.0x
-    spi.setClockSpeed(4000000); //Set SPI bus speed to 4MHz (16MHz / 4 = 4MHz)
-
-    var MP3Clock = readSciRegister16(SCI_CLOCKF);
-    console.log("SCI_ClockF = 0x", MP3Clock.toString(16));
-  }
-
-  function playSample ()
-  {
-    console.log('Loading mp3');
-    var hair = fs.readFileSync('/app/sample.mp3');
-
-    console.log('chunking', hair.length, 'bytes.');
-
-    var streaming = false;
-
-    if (streaming) {
-      writeSciRegister16(SCI_MODE, 0x4800 | (1 << 6));
+  // Reset the mp3 decoder
+  this._softReset(function(err) {
+    if (err) { self._failConnect(err, callback); }
+    else {
+      // Make sure we can comm and have the right version
+      self._checkVersion(function(err) { 
+        if (err) { self._failConnect(err, callback); }
+        else {
+          // Set the clock speed higher
+          self._setClockSpeeds(function(err) {
+            if (err) { self._failConnect(err, callback); }
+            else {
+              // Enabke headphones and lineIn 
+              self.setDefaultIO(function(err) {
+                if (err) { self._failConnect(err, callback); }
+                else {
+                  // Call the callback
+                  callback && callback(null, self);
+                  // Ready the event
+                  setImmediate(function() {
+                    self.emit('ready');
+                  });
+                } 
+              });
+            }
+          });
+        }
+      });
     }
+  });
+}
 
-    var len = hair.length;
-    var chunks = [], clen = streaming ? 512 : 32;
-    var p = 0, i = 0;
-    while (p < len) {
-      chunks[i] = hair.slice(p, p + clen);
-      i = i + 1;
-      p = p + clen;
-      // console.log(p);
-      // tm.pin_write_digital(MP3_DCS, tm.PIN_LOW);
-      // spi.transferSync(hair.substr(p, 32));
-      // tm.pin_write_digital(MP3_DCS, tm.PIN_HIGH);
+Audio.prototype._failConnect = function(err, callback) {
+  setImmediate(function() {
+    this.emit('error', err);
+  }.bind(this))
+
+  return callback && callback(err);
+}
+
+Audio.prototype._softReset = function(callback) {
+  this._readSciRegister16(SCI_MODE, function(err, mode) {
+    if (err) { return callback && callback(err); }
+    else {
+      this._writeSciRegister16(SCI_MODE, mode | 2, function(err) {
+        if (err) { return callback && callback(err); }
+        else {
+          while (!this.MP3_DREQ);
+          this._writeSciRegister16(SCI_MODE, 0x4800, callback);
+        }
+      });
     }
+  }.bind(this))
+}
 
-    console.log('done chunking:', chunks.length, 'chunks.');
-
-    MP3_DCS.low();
-    var i = 0, len = chunks.length;
-    while (i < len) {
-      // while (!MP3_DREQ.read()) { }
-      spi.sendSync(chunks[i]);
-      i = i + 1;
+Audio.prototype._checkVersion = function(callback) {
+  this._readSciRegister16(SCI_STATUS, function(err, MP3Status) { 
+    if (err) { return callback && callback(err); }
+    else if ((MP3Status >> 4) & 0x000F != 4){
+      return callback && callback(new Error("Invalid version returned from module."));
     }
-    console.log('done playing.');
-  }
-
-  function _recordPatch ()
-  {
-    console.log('Patching...')
-    writeSciRegister16(SCI_WRAMADDR, 0x8010);
-    writeSciRegister16(SCI_WRAM, 0x3e12);
-    writeSciRegister16(SCI_WRAM, 0xb817);
-    writeSciRegister16(SCI_WRAM, 0x3e14);
-    writeSciRegister16(SCI_WRAM, 0xf812);
-    writeSciRegister16(SCI_WRAM, 0x3e01);
-    writeSciRegister16(SCI_WRAM, 0xb811);
-    writeSciRegister16(SCI_WRAM, 0x0007);
-    writeSciRegister16(SCI_WRAM, 0x9717);
-    writeSciRegister16(SCI_WRAM, 0x0020);
-    writeSciRegister16(SCI_WRAM, 0xffd2);
-    writeSciRegister16(SCI_WRAM, 0x0030);
-    writeSciRegister16(SCI_WRAM, 0x11d1);
-    writeSciRegister16(SCI_WRAM, 0x3111);
-    writeSciRegister16(SCI_WRAM, 0x8024);
-    writeSciRegister16(SCI_WRAM, 0x3704);
-    writeSciRegister16(SCI_WRAM, 0xc024);
-    writeSciRegister16(SCI_WRAM, 0x3b81);
-    writeSciRegister16(SCI_WRAM, 0x8024);
-    writeSciRegister16(SCI_WRAM, 0x3101);
-    writeSciRegister16(SCI_WRAM, 0x8024);
-    writeSciRegister16(SCI_WRAM, 0x3b81);
-    writeSciRegister16(SCI_WRAM, 0x8024);
-    writeSciRegister16(SCI_WRAM, 0x3f04);
-    writeSciRegister16(SCI_WRAM, 0xc024);
-    writeSciRegister16(SCI_WRAM, 0x2808);
-    writeSciRegister16(SCI_WRAM, 0x4800);
-    writeSciRegister16(SCI_WRAM, 0x36f1);
-    writeSciRegister16(SCI_WRAM, 0x9811);
-    writeSciRegister16(SCI_WRAMADDR, 0x8028);
-    writeSciRegister16(SCI_WRAM, 0x2a00);
-    writeSciRegister16(SCI_WRAM, 0x040e);
-    console.log('Done patching.')
-  }
-
-  function record ()
-  {
-    writeSciRegister16(SCI_AICTRL0, 8000);
-    writeSciRegister16(SCI_AICTRL1, 0);
-    writeSciRegister16(SCI_AICTRL2, 4096);
-    writeSciRegister16(SCI_AICTRL3, 0, 0);
-    writeSciRegister16(SCI_MODE, readSciRegister16(SCI_MODE) | (0 << 14) | (1 << 12) | (1 << 2));
-
-    _recordPatch();
-
-    while (true) {
-      console.log(readSciRegister16(SCI_HDAT0), readSciRegister16(SCI_HDAT1));
-      // writeSciRegister16(SCI_WRAMADDR, 0xC044);
-      // console.log(readSciRegister16(SCI_WRAM))
+    else {
+      return callback && callback();
     }
+  }.bind(this));
+}
+
+Audio.prototype._setClockSpeeds = function(callback) {
+  // Set multiplier to 3.0x
+  this._writeSciRegister16(SCI_CLOCKF, 0x6000, function(err) { 
+    if (err) { return callback && callback(err); }
+    else {
+      this.spi.setClockSpeed(4000000);
+      return callback && callback();
+    }
+  }.bind(this)); 
+
+}
+
+Audio.prototype._SPItransferByte = function(byte, callback) {
+  this._SPItransferArray([byte], function(err, ret) {
+    callback && callback(err, ret[0]);
+  });
+}
+
+Audio.prototype._SPItransferArray = function(array, callback) {
+  this.spi.transfer(new Buffer(array), function(err, ret) {
+    return callback && callback(err, ret);
+  });
+}
+
+//Read the 16-bit value of a VS10xx register
+Audio.prototype._readSciRegister16 = function(addressbyte, callback) {
+
+  // TODO: Use a GPIO interrupt
+  while (!this.MP3_DREQ.read()) ; //Wait for DREQ to go high indicating IC is available
+  this.MP3_XCS.low(); //Select control
+
+  //SCI consists of instruction byte, address byte, and 16-bit data word.
+  // TODO: Error handling
+  this._SPItransferByte(0x03, function(err) {
+    this._SPItransferByte(addressbyte, function(err) {
+      this._SPItransferByte(0xFF, function(err, response1) {
+        // TODO: Use a GPIO interrupt
+        while (!this.MP3_DREQ.read()) ; //Wait for DREQ to go high indicating command is complete
+        this._SPItransferByte(0xFF, function(err, response2) {
+          while (!this.MP3_DREQ.read()) ; //Wait for DREQ to go high indicating command is complete
+
+          this.MP3_XCS.high(); //Deselect Control
+
+          var result = (response1 << 8) + response2;
+
+          callback && callback(err, result)
+
+        }.bind(this));
+      }.bind(this));
+    }.bind(this));
+  }.bind(this));
+}
+
+Audio.prototype._writeSciRegister = function(addressbyte, highbyte, lowbyte, callback) {
+
+  while(!this.MP3_DREQ.read()) ; //Wait for DREQ to go high indicating IC is available
+  this.MP3_XCS.low(); //Select control
+
+  //SCI consists of instruction byte, address byte, and 16-bit data word.
+  this._SPItransferArray([0x02, addressbyte, highbyte, lowbyte], function(err) {
+    if (err) {
+      return callback && callback(err);
+    }
+    else {
+      // TODO: GPIO Interrupt
+      while(!this.MP3_DREQ.read()) ; //Wait for DREQ to go high indicating command is complete
+      this.MP3_XCS.high(); //Deselect Control
+      callback && callback();
+    }
+  }.bind(this))
+}
+
+Audio.prototype._writeSciRegister16 = function(addressbyte, word, callback) {
+  this._writeSciRegister(addressbyte, (word >> 8) & 0xFF, word & 0xFF, callback);
+}
+
+Audio.prototype._setChipGpioDir = function(arg, callback) {
+  this._writeSciRegister16(SCI_WRAMADDR, GPIO_DIR_ADDR, function(err) {
+    if (err) { return callback && callback(err); }
+    else {
+      this._writeSciRegister16(SCI_WRAM, arg, callback);
+    }
+  }.bind(this));
+}
+
+Audio.prototype._setChipGpio = function(arg, callback) {
+  this._writeSciRegister16(SCI_WRAMADDR, GPIO_SET_ADDR, function(err) {
+    this._writeSciRegister16(SCI_WRAM, arg, callback);
+  }.bind(this));
+}
+
+Audio.prototype._getChipGpioDir = function(callback) {
+  this._getChipGPIOValue(GPIO_DIR_ADDR, callback);
+}
+
+Audio.prototype._getChipGpio = function(callback) {
+  this._getChipGPIOValueFromAddr(GPIO_READ_ADDR, callback);
+}
+
+Audio.prototype._getChipGPIOValueFromAddr = function(gpioValue, callback) {
+  this._writeSciRegister16(SCI_WRAMADDR, gpioValue, function(err) {
+    if (err) { return callback && callback(err); }
+    else {
+      this._readSciRegister16(SCI_WRAM, function(err, result) {
+        if (err) { return callback && callback(err); }
+        else {
+          return callback && callback(null, result);
+        }
+      });
+    }
+  }.bind(this));
+}
+
+Audio.prototype._enableAudioOutput = function(callback) {
+  this._setChipGpioDir((1 << 7) + (1 << 5), callback);
+}
+
+Audio.prototype.setDefaultIO = function(callback) {
+  var self = this;
+  self._enableAudioOutput(function(err) {
+    if (err) { self._failConnect(err, callback); }
+    else {
+      self.setInput('microphone', function(err) {
+        if (err) { self._failConnect(err, callback); }
+        else {
+          self.setOutput('headphones', function(err) {
+            if (err) { self._failConnect(err, callback); }
+            else {
+              callback && callback();
+            }
+          });
+        }
+      });
+    }
+  });
+}
+
+Audio.prototype.setVolume = function(leftChannel, rightChannel) {
+  // Set VS10xx Volume Register
+  this._writeSciRegister(SCI_VOL, leftChannel, rightChannel, callback);
+}
+
+Audio.prototype.setInput = function(input, callback) {
+  if (input != 'lineIn' && input != 'microphone') {
+    return callback && callback(new Error("Invalid input requested..."));
   }
+  else {
+    this.input = input;
 
-  // Some fun initialization
-  initialize();
+    var bit = (input == 'microphone' ? 1 : 0);
 
-  return {
-    enableHeadphones: enableHeadphones,
-    enableLineOut: enableLineOut,
-    enableMicrophone: enableMicrophone,
-    enableLineIn: enableLineIn,
-
-    // gross
-    playSample: playSample,
-    record: record
+    this._getChipGpio(function(err, gpio) {
+      if (err) { return callback && callback(err); }
+      else {
+        this._setChipGpio(gpio & (bit << 5), callback);
+      }
+    }.bind(this));
   }
 }
 
-exports.connect = connect;
+Audio.prototype.setOutput = function(output, callback) {
+  if (output != 'lineOut' && output != 'headphones') {
+    return callback && callback(new Error("Invalid output requested..."));
+  }
+  else {
+    this.output = output;
+
+    var bit = (output == 'lineOut' ? 1 : 0);
+
+    this._getChipGpio(function(err, gpio) {
+      if (err) { return callback && callback(err); }
+      else {
+        this._setChipGpio(gpio & (bit << 7), callback);
+      }
+    }.bind(this));
+  }
+}
+
+Audio.prototype.play = function(buff, callback) {
+  console.log('Loading mp3');
+
+  console.log('chunking', buff.length, 'bytes.');
+
+
+  var len = buff.length;
+  var chunks = [], clen =  32;
+  var p = 0, i = 0;
+  while (p < len) {
+    chunks[i] = buff.slice(p, p + clen);
+    i = i + 1;
+    p = p + clen;
+  }
+
+  console.log('done chunking:', chunks.length, 'chunks.');
+
+  this.MP3_DCS.low();
+  var i = 0, len = chunks.length;
+  while (i < len) {
+    // while (!MP3_DREQ.read()) { }
+    this.spi.sendSync(chunks[i]);
+    i = i + 1;
+  }
+  console.log('done playing.');
+}
+exports.use = use;
--- test.js
+++ test.js
@@ -1,9 +1,13 @@
 var tessel = require('tessel');
+var fs = require('fs');
+var audio = require('./').use(tessel.port('a'));
 
-var audio = require('./').connect(tessel.port('a'));
+audio.on('ready', function() {
+  console.log("Ready to go!");
+  var song = fs.readFileSync('/app/sample.mp3');
+  audio.play(song);
+});
 
-// audio.enableHeadphones();
-// audio.enableLineOut();
-audio.playSample();
-
-// audio.record();
+audio.on('error', function(err) {
+  console.log("Failed to connect", err);
+})
