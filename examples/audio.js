b4b88f7fcd3f639841248cf0f5cc47b3543e186e
johnnyman727 <johnnyman727@gmail.com>
2014-05-14 16:55:55 -0700

    Adding example repo

---
+++ examples/audio.js
@@ -0,0 +1,34 @@
+var tessel = require('tessel');
+var fs = require('fs');
+var audio = require('../').use(tessel.port('a'));
+
+console.log('trying to connect...');
+audio.on('ready', function() {
+  console.log("Ready to go!");
+  audio.setVolume(2, 2, function(err) {
+    if (err) return console.log('err setting volume', err);
+    var song = fs.readFileSync('/app/sample.mp3');
+    audio.play(song, function(err) {
+      if (err) {
+        console.log("error playing song: ", err);
+      }
+      else {
+        console.log("Done playing the first song");
+      }
+    });
+    // audio.play(song, function(err) {
+    //   if (err) {
+    //     console.log("error playing song: ", err);
+    //   }
+    //   else {
+    //     console.log("Done playing the second song");
+    //   }
+    // });
+  })
+});
+
+audio.on('error', function(err) {
+  console.log("Failed to connect", err);
+});
+
+setInterval(function(){}, 20000);
