23c8678300353664923967735e56dabdf40baaac
johnnyman727 <johnnyman727@gmail.com>
2014-06-04 22:35:15 -0700

    Fixes playback example

--- examples/playback.js
+++ examples/playback.js
@@ -14,32 +14,43 @@ var audio = require('../').use(tessel.port['A']); // Replace '../' with 'audio-v
 
 var chunks = [];
 
-// When we get data, throw it in our array
-audio.on('data', chunks.push.bind(chunks));
+// When we get data, push it into our array
+audio.on('data', function(data) {
+  chunks.push(data);
+});
 
 // Wait for the module to connect
 audio.on('ready', function() {
   console.log('Hold the config button to record...');
   // When the config button is pressed, start recording
-  tessel.button.once('press', audio.startRecording.bind(audio, 'voice', null));
-
-  // When the config button is released
-  tessel.button.on('release', function() {
-    console.log('stopping recording');
-    // Stop recording
-    audio.stopRecording(function() {
-      console.log('Playing it back...');
-      // Concat the data and play it
-      audio.play(Buffer.concat(chunks), function(err) {
-        // When we're done playing, clear recordings
-        chunks = [];
-        console.log('Hold the config button to record...');
-        // Wait for a button press again
-        tessel.button.once('press', audio.startRecording.bind(audio, 'voice', null));
-      });
-    });
-  })
+  tessel.button.once('press', startRecording);
 });
+
+
+function startRecording() {
+  // Tell the audio module to start recording
+  audio.startRecording('voice', function() {
+    console.log('Recording...');
+    // Once the button is released, stop recording
+    tessel.button.once('release', stopRecording);
+  });
+}
+
+function stopRecording() {
+  // Tell the audio module to stop recording
+  console.log('stopping the recording...');
+  audio.stopRecording(function() {
+    console.log('Playing it back...');
+    // Concat the data and play it
+    audio.play(Buffer.concat(chunks), function(err) {
+      // When we're done playing, clear recordings
+      chunks = [];
+      console.log('Hold the config button to record...');
+      // Wait for a button press again
+      tessel.button.once('press', startRecording);
+    });
+  });
+}
 
 
 // If there is an error, report it
